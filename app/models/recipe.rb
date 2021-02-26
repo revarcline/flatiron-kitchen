29d56a6b1b572d458dc44141b223bf10c5d9cad2
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-25 22:08:39 -0600

    commit tree dde1146dcfb1bc0a00fe07e0e77c89f1c2cb8a7c

---
+++ app/models/recipe.rb
@@ -0,0 +1,8 @@
+class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  validates :name, presence: true
+
+  accepts_nested_attributes_for :ingredients
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@
 # http://www.foodnetwork.com/recipes/trisha-yearwood/blueberry-pancakes-recipe.html
 
 blueberry_pancakes = Recipe.new(name: 'Blueberry Pancakes')
-blueberry_pancakes_ingredients = ['All-purpose flour', 'Sugar', 'Baking powder', 'Baking soda', 'Salt', 'Large eggs', 'Milk', 'Sour cream', 'Butter', 'Vanilla extract', 'Blueberries', 'Lemon zest']
-blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(name: i)}
+blueberry_pancakes_ingredients = ['All-purpose flour', 'Sugar', 'Baking powder', 'Baking soda', 'Salt', 'Large eggs',
+                                  'Milk', 'Sour cream', 'Butter', 'Vanilla extract', 'Blueberries', 'Lemon zest']
+blueberry_pancakes_ingredients.each { |i| blueberry_pancakes.ingredients.build(name: i) }
 blueberry_pancakes.save
 
 # Jake the Dog's Bacon Pancakes Recipe
@@ -11,5 +12,5 @@ blueberry_pancakes.save
 
 bacon_pancakes = Recipe.new(name: 'Bacon Pancakes')
 bacon_pancakes_ingredients = ['Some bacon', 'Pancake']
-bacon_pancakes_ingredients.each{|i| bacon_pancakes.ingredients.build(name: i)}
+bacon_pancakes_ingredients.each { |i| bacon_pancakes.ingredients.build(name: i) }
 bacon_pancakes.save
